d3a82772d652e6b34425724cc9e035c93a6ee74b
yzzhan <zhanyingzi1988@gmail.com>
2016-04-20 12:29:56 +0800

    fix bug

--- app/components/question.js
+++ app/components/question.js
@@ -87,7 +87,7 @@ class Question extends React.Component{
 
     choose(index){
         const{question,onChoose} = this.props;
-        $("h2.page-header").focus();
+        $("h2.page-header").click();
         onChoose(question._id,this.convertAnswer(index));
     }
 
